ec789acecbdca0c9326ba4c7be4cbe6fe4650cb1
jonas7414 <jonas_7777@icloud.com>
2026-05-11 13:32:24 +0800

    Initial Commit

---
+++ iqos_tool/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iqos_tool
+//
+//  Created by 陳阿頡 on 2026/5/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iqos_tool/iqos_toolApp.swift
@@ -0,0 +1,17 @@
+//
+//  iqos_toolApp.swift
+//  iqos_tool
+//
+//  Created by 陳阿頡 on 2026/5/11.
+//
+
+import SwiftUI
+
+@main
+struct iqos_toolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
